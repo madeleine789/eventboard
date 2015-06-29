81423d2a0e4d99e6ac17c2d1b84be53b0cb881cf
madeleine789 <madeleine789@github.com>
2015-06-29 15:01:20 +0200

    profiler setup

--- profile.py
+++ profile.py
@@ -1,8 +1,21 @@
+from views.auth import auth_app
+from views.events import events_app
+from views.users import users_app
+
 __author__ = 'mms'
 
 from werkzeug.contrib.profiler import ProfilerMiddleware
-from app import app
+from app import app, cache
+
+app.register_blueprint(events_app)
+app.register_blueprint(auth_app)
+app.register_blueprint(users_app)
 
 app.config['PROFILE'] = True
 app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
-app.run(debug=True)
+if __name__ == "__main__":
+
+	app.run( debug=True)
+
+	with app.app_context():
+		cache.clear()
